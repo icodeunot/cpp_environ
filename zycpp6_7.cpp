64396f533f4f37cd3a204364a99e740b6fa3137f
js <89633614+icodeunot@users.noreply.github.com>
2022-01-26 22:00:05 -0800

    c867 Ch. 6.7.1
    Read integers from input and store each integer into a vector until -1 is read. Do not store -1 into the vector. Then, if the last value in the vector is negative, output the negative values in the vector, each on a new line. Otherwise, output all values in the vector that are not negative, each on a new line.
    
    Ex: If the input is -81 36 -74 2 -1, the output is:
    
    36
    2

---
+++ zycpp6_7.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+   int input;
+   int i;
+
+   vector<int> intVect;
+   
+   cin >> input;
+   
+   while (input != -1) {
+      intVect.push_back(input);
+      cin >> input;
+   }
+   
+   
+   if (intVect.at(intVect.size() - 1) < 0) {
+      for (i = 0; i < intVect.size(); ++i) {
+         if (intVect.at(i) < 0) {
+            cout << intVect.at(i)
+                 << endl;
+         }
+      }
+   }
+   else if (intVect.at(intVect.size() - 1) > 0) {
+      for (i = 0; i < intVect.size(); ++i) {
+         if (intVect.at(i) > 0) {
+            cout << intVect.at(i)
+                 << endl;
+         }
+      }
+   }
+
+   return 0;
+}
+
